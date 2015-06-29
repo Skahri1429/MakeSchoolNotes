6890c1e582b118ef6f0f2246a9c99877be4c3181
Sarthak Khillon <sarthakkhillon24@gmail.com>
2015-06-29 10:52:33 -0700

    Completed Make School Notes edition downloaded from Make School; not my version

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let modernProgrammingLanguage = "Swift"
+
+var potato : String = "oooo"
+
+var language = "Swift"
+
